ccc9cbd7eac322100da35a78729c304288dd9d63
drecksposer <32773076+drecksposer@users.noreply.github.com>
2022-05-30 12:19:36 +0200

    Update graphs.R

--- graphs.R
+++ graphs.R
@@ -55,7 +55,7 @@ plot_dominance_btc_eth <- function(){
   ggplot(btc.eth, aes(fill=index_members, y=weights, x=date)) + 
     geom_bar(position="stack", stat="identity")+
     scale_fill_viridis(discrete = T, begin = 0.5) +
-    scale_x_date(limits = c(as.Date("2020-07-01"), as.Date("2021-06-01")))+
+    #scale_x_date(limits = c(as.Date("2020-07-01"), as.Date("2021-06-01")))+
     theme(
       panel.background = element_rect(fill='transparent'), #transparent panel bg
       plot.background = element_rect(fill='transparent', color=NA), #transparent plot bg
